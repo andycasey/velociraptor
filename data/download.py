1f91a20af160045d5891f6185ed3896294aad8d7
Andy Casey <andycasey@gmail.com>
2018-05-07 18:40:35 +1000

    use a subset of the data

--- data/download.py
+++ data/download.py
@@ -23,6 +23,29 @@ job = Gaia.launch_job_async("""
     AND     rv_nb_transits > 10
     AND     visibility_periods_used > 10
     AND     radial_velocity_error < 20
+    AND     MOD(random_index, 10) == 0
     """)
-results = job.get_results()
-results.write("rv_floor_cal-result.fits")
+subset = job.get_results()
+subset.write("rv_floor_cal_subset-result.fits")
+
+
+job = Gaia.launch_job_async("""
+    SELECT  source_id,
+            parallax, parallax_error,
+            phot_g_mean_mag,
+            phot_g_mean_flux, phot_g_mean_flux_error,
+            phot_bp_mean_flux, phot_bp_mean_flux_error, phot_bp_n_obs,
+            phot_rp_mean_flux, phot_rp_mean_flux_error, phot_rp_n_obs,
+            phot_bp_rp_excess_factor,
+            bp_rp, bp_g, g_rp,
+            radial_velocity, radial_velocity_error, rv_nb_transits,
+            astrometric_weight_al, astrometric_gof_al, astrometric_chi2_al
+    FROM    gaiadr2.gaia_source
+    WHERE   radial_velocity IS NOT NULL
+    AND     duplicated_source = 'false'
+    AND     rv_nb_transits > 10
+    AND     visibility_periods_used > 10
+    AND     radial_velocity_error < 20
+    """)
+sources = job.get_results()
+sources.write("rv_floor_cal-result.fits")
