aba4b6f742de8629147e36b4a4704b21dabbd5cb
Andy Casey <andycasey@gmail.com>
2018-06-13 17:14:30 +1000

    do optimisation on subset, even tho we are fixing mu_b

--- npm.py
+++ npm.py
@@ -15,13 +15,19 @@ from sklearn import neighbors as neighbours
 
 import velociraptor
 import stan_utils as stan
+import npm_utils
 
-np.random.seed(42)
+from corner import corner
+
+np.random.seed(123)
 
 
 # 
 data_path = "data/rv-all-subset-1e4.fits"
-data = fits.open(data_path)[1].data
+#data = fits.open(data_path)[1].data
+data = Table.read(data_path)
+data["rv_single_epoch_scatter"] = data["rv_single_epoch_variance"]**0.5
+
 
 
 # [1] Construct KD-Tree
@@ -34,15 +40,16 @@ data = fits.open(data_path)[1].data
 # what labels are we going to build the KD-tree in?
 kdt_label_names = ("bp_rp", "absolute_rp_mag", "phot_rp_mean_mag")
 
-predictor_label_names = (
-    "rv_single_epoch_variance",
 
+predictor_label_names = (
+    "rv_single_epoch_scatter",
+    "astrometric_unit_weight_error",
+    "phot_bp_rp_excess_factor",
+    "rv_abs_diff_template_teff",
 )
-#
-#    "rv_abs_diff_template_teff",
-#    "astrometric_unit_weight_error",
-#    "phot_bp_rp_excess_factor",
-#)
+
+parameter_names = ["theta", "mu_single", "sigma_single", "mu_multiple",
+    "sigma_multiple"]
 
 
 X_kdt = np.vstack([data[ln] for ln in kdt_label_names]).T
@@ -61,172 +68,80 @@ _descale = lambda a: a * X_scale + X_mean
 X_norm = _scale(X_kdt)
 
 # Construct the KD-Tree
-kdt_kwds = dict(leaf_size=40, metric="minkowski")
-kdt = neighbours.KDTree(X_norm, **kdt_kwds)
+kdt = npm_utils.build_kdt(X_norm)
 
 data = data[subset]
-
-
-def get_ball_around_point(index, K=1000, full_output=False):
-    dist, k_indices = kdt.query(X_norm[[index]], K)
-    dist, k_indices = (dist[0], k_indices[0])
-    return (k_indices, dist) if full_output else k_indices
 
 
 model = stan.load_stan_model("npm.stan")
 
 # Calculate the total number of parameters
 M = len(data)
-K = 1 + 2 * len(predictor_label_names)
-
-
-# Stan needs a finite value to initialize correctly, so we will use a dumb (more
-# robust) optimizer to get an initialization value.
-def norm_pdf(x, norm_mu, norm_sigma, theta):
-    return theta * (2 * np.pi * norm_sigma**2)**(-0.5) * np.exp(-(x - norm_mu)**2/(2*norm_sigma**2))
-    
-
-def lognorm_pdf(x, lognorm_mu, lognorm_sigma, theta):
-    return (1.0 - theta)/(x * lognorm_sigma * np.sqrt(2*np.pi)) \
-           * np.exp(-0.5 * ((np.log(x) - lognorm_mu)/lognorm_sigma)**2)
-
-
-def ln_likelihood(y, theta, s_mu, s_sigma, b_mu, b_sigma):
-    
-    s_ivar = s_sigma**-2
-    b_ivar = b_sigma**-2
-    hl2p = 0.5 * np.log(2*np.pi)
-    
-    s_lpdf = np.log(theta) - hl2p + 0.5 * np.log(s_ivar) \
-           - 0.5 * (y - s_mu)**2 * s_ivar
-    
-    b_lpdf = np.log(1 - theta) - np.log(y*b_sigma) - hl2p \
-           - 0.5 * (np.log(y) - b_mu)**2 * b_ivar
-    ll = np.sum(s_lpdf) + np.sum(b_lpdf)
-    #print(lpdf)
-    
-    assert np.isfinite(ll)
-    return ll
-
-
-def ln_prior(theta, s_mu, s_sigma, b_mu, b_sigma):
-
-    # Ensure that the *mode* of the log-normal distribution is larger than the
-    # mean of the normal distribution
-    min_mu_multiple = np.log(s_mu) + b_sigma**2 + s_sigma
-
-    if not (1 >= theta >= 0) \
-    or np.any(s_mu <= 0) \
-    or np.any(s_sigma <= 0) \
-    or np.any(b_sigma <= 0) \
-    or np.any(b_mu <= min_mu_multiple):
-        return -np.inf
-    return 0
-
-
-def ln_prob(y, L, *params):
-    theta, s_mu, s_sigma, b_mu, b_sigma = _unpack_params(params, L=L)
-    lp = ln_prior(theta, s_mu, s_sigma, b_mu, b_sigma)
-    if np.isfinite(lp):
-        return lp + ln_likelihood(y, theta, s_mu, s_sigma, b_mu, b_sigma)
-    return lp
-
-
-def _unpack_params(params, L):
-    # unpack the multdimensional values.
-    theta = params[0]
-    mu_single = np.array(params[1:1 + L])
-    sigma_single = np.array(params[1 + L:1 + 2 * L])
-    mu_multiple = np.array(params[1 + 2 * L:1 + 3 * L])
-    sigma_multiple = np.array(params[1 + 3 * L:1 + 4 * L])
-
-    return (theta, mu_single, sigma_single, mu_multiple, sigma_multiple)
-
-
-def _pack_params(theta, mu_single, sigma_single, mu_multiple, sigma_multiple):
-    return np.hstack([theta, mu_single, sigma_multiple, mu_multiple, sigma_multiple])
-
-
-
-
-def nlp(params, y, L):
-    return -ln_prob(y, L, *params)
-
-
-
-def get_initialization_point(y):
-    N, D = y.shape
-
-    init_dict = dict(
-        theta=0.5,
-        mu_single=np.median(y, axis=0),
-        sigma_single=0.1 * np.median(y, axis=0),
-        sigma_multiple=1e-2 * np.ones(D),
-    )
-    init_dict.update(
-        mu_multiple=1e-3 + np.log(init_dict["mu_single"]) \
-                    + init_dict["sigma_multiple"]**2 \
-                    + init_dict["sigma_single"])
-
-    x0 = _pack_params(**init_dict)
-    L = len(predictor_label_names)
-
-    op_kwds = dict(x0=x0, args=(y, L))
-
-    p_opt = op.minimize(nlp, **op_kwds)
-
-    # make no checks
-    return p_opt.x
-
-
-
-init_theta = 0.5
-init_sigma_multiple = 0.1
-mu_multiple_factor = 1. + 1e-2
+K = 1 + 4 * len(predictor_label_names)
 
 opt_params = np.empty((M, K))
 
-parameter_names = ["theta", "mu_single", "sigma_single", "mu_multiple",
-    "sigma_multiple"]
+subset_points = np.random.choice(M, size=1000, replace=False)
+#for i in range(M):
 
-for i in range(M):
+for j, i in enumerate(subset_points):
 
-    k_indices, dist = get_ball_around_point(i, full_output=True)
+    print("At point {}/{}: {}".format(j, M, i))
+
+    k_indices, dist = npm_utils.get_ball_around_point(kdt, X_norm[[i]],
+                                                      full_output=True)
 
     y = np.array([data[ln][k_indices] for ln in predictor_label_names]).T
-
-    L = len(predictor_label_names)
-
-    init_values = get_initialization_point(y)
-    init_dict = dict(zip(parameter_names, _unpack_params(init_values, L)))
-
-    fig, axes = plt.subplots(1, L)
-    axes = np.atleast_1d(axes).flatten()
-
-    for j, ax in enumerate(axes):
-        ax.hist(y.T[j])
-
-        #ax.set_title(predictor_label_names[j])
-
     N, D = y.shape
+    
+    init_values = npm_utils.get_initialization_point(y)
+    init_dict = dict(zip(parameter_names, npm_utils._unpack_params(init_values, D)))  
+
+    data_dict = dict(y=y, N=N, D=D)
+
     opt_kwds = dict(
-        data=dict(y=y, N=N, D=D, max_y=np.max(y, axis=0)),
+        data=data_dict,
         init=init_dict,
-        iter=100000, 
+        verbose=False,
         tol_obj=7./3 - 4./3 - 1, # machine precision
         tol_grad=7./3 - 4./3 - 1, # machine precision
         tol_rel_grad=1e3,
         tol_rel_obj=1e4)
-
-
-
+    
     p_opt = model.optimizing(**opt_kwds)
+    
     for k in p_opt.keys():
         if k == "theta":  continue
         p_opt[k] = np.atleast_1d(p_opt[k])
 
+    opt_params[i] = npm_utils._pack_params(**p_opt)
+
+    print("Single star fraction at this point: {:.2f}".format(opt_params[i, 0]))
+
+
+    """
+    for l, predictor_label_name in enumerate(predictor_label_names):
+
+        fig, ax = plt.subplots()
+        xi = np.linspace(
+            np.min(data_dict["y"].T[l].flatten()), 
+            np.max(data_dict["y"].T[l].flatten()),
+            1000)
+
+        ax.hist(data_dict["y"].T[l].flatten(), bins=50, facecolor="#cccccc", zorder=-1)
+
+
+        show = init_dict
+        ax.plot(xi, N * npm_utils.norm_pdf(xi, show["mu_single"][l], show["sigma_single"][l], show["theta"]), c='r')
+        ax.plot(xi, N * npm_utils.lognorm_pdf(xi, show["mu_multiple"][l], show["sigma_multiple"][l], show["theta"]), c='b')
+
+        show = p_opt
+        ax.plot(xi, N * npm_utils.norm_pdf(xi, show["mu_single"][l], show["sigma_single"][l], show["theta"]), c='m')
+        ax.plot(xi, N * npm_utils.lognorm_pdf(xi, show["mu_multiple"][l], show["sigma_multiple"][l], show["theta"]), c='y')
+        
+            
     samples  = model.sampling(**stan.sampling_kwds(
-        data=opt_kwds["data"], init=p_opt, iter=10000, chains=2))
+        data=opt_kwds["data"], init=p_opt, iter=2000, chains=2))
 
     chains_dict = samples.extract()
     if L == 1:
@@ -248,41 +163,39 @@ for i in range(M):
         ])
 
 
+    fig = corner(chains)
+
     # Make plots of the pdf of each distribution.
+    for l, predictor_label_name in enumerate(predictor_label_names):
 
-    def norm_pdf(x, norm_mu, norm_sigma, theta):
-        return theta * (2 * np.pi * norm_sigma**2)**(-0.5) * np.exp(-(x - norm_mu)**2/(2*norm_sigma**2))
-        
-    def lognorm_pdf(x, lognorm_mu, lognorm_sigma, theta):
-        return (1.0 - theta)/(x * lognorm_sigma * np.sqrt(2*np.pi)) \
-               * np.exp(-0.5 * ((np.log(x) - lognorm_mu)/lognorm_sigma)**2)
+        xi = np.linspace(np.min(y.T[l]), np.max(y.T[l]), 1000)
 
-    xi = np.linspace(np.min(y.T[0]), np.max(y.T[0]), 1000)
+        indices = np.random.choice(len(chains), 100, replace=False)
 
-    indices = np.random.choice(len(chains), 100, replace=False)
-        
-    fig, ax = plt.subplots()
-    for index in indices:
-        
-        idx_theta = parameter_names.index("theta")
-        idx_norm_mu = parameter_names.index("mu_single")
-        idx_norm_sigma = parameter_names.index("sigma_single")
-        idx_lognorm_mu = parameter_names.index("mu_multiple")
-        idx_lognorm_sigma = parameter_names.index("sigma_multiple")
-        
-        theta = chains[index, idx_theta]
-        norm_mu = chains[index, idx_norm_mu]
-        norm_sigma = chains[index, idx_norm_sigma]
-        lognorm_mu = chains[index, idx_lognorm_mu]
-        lognorm_sigma = chains[index, idx_lognorm_sigma]
-        
-        ax.plot(xi, norm_pdf(xi, norm_mu, norm_sigma, theta), c='r', alpha=0.1)
-        ax.plot(xi, lognorm_pdf(xi, lognorm_mu, lognorm_sigma, theta), c='b', alpha=0.1)
-        
-    _ = ax.hist(y, bins=500, facecolor="#000000", zorder=-1, normed=True)
-    print(y.size)
+        fig, ax = plt.subplots()
+        for index in indices:
+            
+            idx_theta = parameter_names.index("theta")
+            idx_norm_mu = parameter_names.index("mu_single")
+            idx_norm_sigma = parameter_names.index("sigma_single")
+            idx_lognorm_mu = parameter_names.index("mu_multiple")
+            idx_lognorm_sigma = parameter_names.index("sigma_multiple")
+            
 
-    # = model.sampling(**stan.sampling_kwds(data=data_dict, init=init_dict, iter=2000))
-    raise a
+            theta = chains[index, 0]
+            norm_mu = chains[index, 1 + l]
+            norm_sigma = chains[index, 1 + L + l]
+            lognorm_mu = chains[index, 1 + 2*L + l]
+            lognorm_sigma = chains[index, 1 + 3*L + l]
 
+            ax.plot(xi, npm_utils.norm_pdf(xi, norm_mu, norm_sigma, theta), c='r', alpha=0.1)
+            ax.plot(xi, npm_utils.lognorm_pdf(xi, lognorm_mu, lognorm_sigma, theta), c='b', alpha=0.1)
+            
+        _ = ax.hist(y.T[l], bins=500, facecolor="#000000", zorder=-1, normed=True)
+        print(y.size)
+
+        ax.set_title(predictor_label_name.replace("_", " "))
+
+
+    """
 
---
+++ npm_utils.py
@@ -0,0 +1,120 @@
+
+import numpy as np
+from scipy import (optimize as op, stats)
+from sklearn import neighbors as neighbours
+
+import stan_utils as stan
+
+
+def build_kdt(X_norm, **kwargs):
+
+    kdt_kwds = dict(leaf_size=40, metric="minkowski")
+    kdt_kwds.update(kwargs)
+    kdt = neighbours.KDTree(X_norm, **kdt_kwds)
+
+    return kdt
+
+
+def get_ball_around_point(kdt, point, K=1000, full_output=False):
+    dist, k_indices = kdt.query(point, K)
+    dist, k_indices = (dist[0], k_indices[0])
+    return (k_indices, dist) if full_output else k_indices
+
+
+# Stan needs a finite value to initialize correctly, so we will use a dumb (more
+# robust) optimizer to get an initialization value.
+def norm_pdf(x, norm_mu, norm_sigma, theta):
+    return theta * (2 * np.pi * norm_sigma**2)**(-0.5) * np.exp(-(x - norm_mu)**2/(2*norm_sigma**2))
+    
+
+def lognorm_pdf(x, lognorm_mu, lognorm_sigma, theta):
+    return (1.0 - theta)/(x * lognorm_sigma * np.sqrt(2*np.pi)) \
+           * np.exp(-0.5 * ((np.log(x) - lognorm_mu)/lognorm_sigma)**2)
+
+
+def ln_likelihood(y, theta, s_mu, s_sigma, b_mu, b_sigma):
+    
+    s_ivar = s_sigma**-2
+    b_ivar = b_sigma**-2
+    hl2p = 0.5 * np.log(2*np.pi)
+    
+    s_lpdf = np.log(theta) - hl2p + 0.5 * np.log(s_ivar) \
+           - 0.5 * (y - s_mu)**2 * s_ivar
+    
+    b_lpdf = np.log(1 - theta) - np.log(y*b_sigma) - hl2p \
+           - 0.5 * (np.log(y) - b_mu)**2 * b_ivar
+    ll = np.sum(s_lpdf) + np.sum(b_lpdf)
+    #print(lpdf)
+    
+    assert np.isfinite(ll)
+    return ll
+
+
+def ln_prior(theta, s_mu, s_sigma, b_mu, b_sigma):
+
+    # Ensure that the *mode* of the log-normal distribution is larger than the
+    # mean of the normal distribution
+    min_mu_multiple = np.log(s_mu) + b_sigma**2
+
+    if not (1 >= theta >= 0) \
+    or np.any(s_mu <= 0) \
+    or np.any(s_sigma <= 0) \
+    or np.any(b_sigma <= 0) \
+    or np.any(b_mu < min_mu_multiple):
+        return -np.inf
+
+    # Beta prior on theta.
+    return stats.beta.logpdf(theta, 5, 5)
+
+
+def ln_prob(y, L, *params):
+    theta, s_mu, s_sigma, b_mu, b_sigma = _unpack_params(params, L=L)
+    lp = ln_prior(theta, s_mu, s_sigma, b_mu, b_sigma)
+    if np.isfinite(lp):
+        return lp + ln_likelihood(y, theta, s_mu, s_sigma, b_mu, b_sigma)
+    return lp
+
+
+def _unpack_params(params, L):
+    # unpack the multdimensional values.
+    theta = params[0]
+    mu_single = np.array(params[1:1 + L])
+    sigma_single = np.array(params[1 + L:1 + 2 * L])
+    mu_multiple = np.array(params[1 + 2 * L:1 + 3 * L])
+    sigma_multiple = np.array(params[1 + 3 * L:1 + 4 * L])
+
+    return (theta, mu_single, sigma_single, mu_multiple, sigma_multiple)
+
+
+def _pack_params(theta, mu_single, sigma_single, mu_multiple, sigma_multiple):
+    return np.hstack([theta, mu_single, sigma_single, mu_multiple, sigma_multiple])
+
+
+
+
+def nlp(params, y, L):
+    return -ln_prob(y, L, *params)
+
+
+
+def get_initialization_point(y):
+    N, D = y.shape
+
+    init_dict = dict(
+        theta=0.5,
+        mu_single=np.median(y, axis=0),
+        sigma_single=0.1 * np.median(y, axis=0),
+        sigma_multiple=1e-2 * np.ones(D),
+    )
+    init_dict["mu_multiple"] = 1.05 * (
+          np.log(init_dict["mu_single"] + 3 * init_dict["sigma_single"]) \
+        + pow(init_dict["sigma_multiple"], 2))
+
+    x0 = _pack_params(**init_dict)
+    
+    op_kwds = dict(x0=x0, args=(y, D))
+
+    p_opt = op.minimize(nlp, **op_kwds)
+
+    # make no checks
+    return p_opt.x
