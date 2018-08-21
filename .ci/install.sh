33d5c318f0bc9bd0ddd8045ab74f8fcc1c2ec2e0
Andy Casey <andycasey@gmail.com>
2018-08-21 20:53:34 +1000

    trigger travis

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -x
 
-# SSL workaround
-#mkdir -p ~/.config/Tectonic
-#cp .ci/tectonic.config.toml ~/.config/Tectonic/config.toml
-
 # Conda install
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
